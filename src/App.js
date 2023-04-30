44cb719fc219ad32b6adb88f30f30095032f197a
mahmoudessam16 <mahmoudemourad29@gmail.com>
2023-04-30 20:58:32 +0300

    modifying

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
+import Home from "./components/Home"
+import About from "./components/About"
 
 function App() {
     return (
