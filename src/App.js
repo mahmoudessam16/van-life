6385e0a12088d39a2032701a318330b3d71ede0a
mahmoudessam16 <mahmoudemourad29@gmail.com>
2023-04-30 21:08:34 +0300

    some edit 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
